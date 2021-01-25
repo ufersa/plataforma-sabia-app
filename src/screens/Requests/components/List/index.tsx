20666e900462320ea97e28a4d56519ee85874d1f
Davi de Oliveira <davioliveira.dev@gmail.com>
2021-01-25 20:04:48 -0300

    Fix List component

--- src/screens/Requests/components/List/index.tsx
+++ src/screens/Requests/components/List/index.tsx
@@ -1,42 +1,47 @@
 import React from 'react';
 import { View, FlatList, ListRenderItemInfo } from 'react-native';
 import styled from 'styled-components';
+import { DefaultText } from '../../../../components';
 import RequestCard from './RequestCard';
 
 interface ListItemProps {
   title: string
   amount: number
   status: string
-};
+}
 
 interface ListProps {
   data: ListItemProps[]
 }
 
 const ListWrapper = styled(View)`
-  margin-top: 34px;
+  margin-top: 18px;
 `;
 
 const ListContainer = styled(FlatList)`
   padding: 16px;
 `;
 
-const List = ({ data }: ListProps): JSX.Element => {
-  return (
-    <ListWrapper>
-      {data && data.length > 0 ? (
-        <ListContainer
-          data={data}
-          renderItem={({ item }: ListRenderItemInfo<any>) => (
-            <RequestCard {...item} />
-          )}
-          keyExtractor={(item, idx) => idx.toString()}
-        />
-      ) : (
-        <View></View>
-      )}
-    </ListWrapper>
-  );
-};
+const Empty = styled(View)`
+  align-items: center;
+`;
+
+const List = ({ data }: ListProps): JSX.Element => (
+  <ListWrapper>
+    {data && data.length > 0 ? (
+      <ListContainer
+        data={data}
+        renderItem={({ item }: ListRenderItemInfo<any>) => (
+          <RequestCard {...item} />
+        )}
+        keyExtractor={(item, idx) => idx.toString()}
+      />
+    ) : (
+      <Empty>
+        <DefaultText>Nenhum pedido</DefaultText>
+      </Empty>
+    )}
+  </ListWrapper>
+);
 
 export default List;
