60120b6c15a157fc545cf8f51037a94c426eda2e
Luiz Cláudio <luizcns18@gmail.com>
2021-02-15 15:59:13 -0300

    Refactor header

--- src/screens/Home/components/Header/index.tsx
+++ src/screens/Home/components/Header/index.tsx
@@ -5,9 +5,18 @@ import Logo from '../../../../../assets/logo/Logo.png';
 
 const Header = (): JSX.Element => (
   <S.Wrapper>
-    <S.Cart />
+    <S.Touch activeOpacity={0.7}>
+      <S.CardWrapper>
+        <S.CardBadge>
+          <S.CardBadgeText>1</S.CardBadgeText>
+        </S.CardBadge>
+        <S.CartIcon size={30} color="#4a4a4a" />
+      </S.CardWrapper>
+    </S.Touch>
     <Image source={Logo} style={{ width: 120, height: 35 }} />
-    <S.User />
+    <S.Touch activeOpacity={0.7}>
+      <S.UserWrapper />
+    </S.Touch>
   </S.Wrapper>
 );
 
