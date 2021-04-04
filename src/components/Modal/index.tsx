2536f8fd366a2a67e95d20acff4882d83ad578aa
davioliveiira <davioliveira.dev@gmail.com>
2021-04-04 14:08:34 -0300

    Contact page

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import { Modal as ModalRN, ModalBaseProps, TouchableWithoutFeedback } from 'react-native';
+import React, { useEffect } from 'react';
+import {
+  Modal as ModalRN,
+  ModalBaseProps,
+  TouchableWithoutFeedback,
+  Animated,
+} from 'react-native';
 import * as S from './styles';
 
 interface ModalProps extends ModalBaseProps {
-  title?: string,
+  title?: string
+  titleStyle?: object
   children: JSX.Element
   onClose(): void
 }
 
 const Modal = ({ children, ...props }: ModalProps): JSX.Element => {
-  const { title = '', onClose } = props;
+  const { title = '', onClose, titleStyle } = props;
+  const opacity = new Animated.Value(0);
+
+  const closeModal = (): void => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+
+    setTimeout(() => {
+      onClose();
+    }, 200);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 200,
+        useNativeDriver: false,
+      }).start();
+    }, 300);
+  }, []);
 
   return (
     <S.Wrapper>
@@ -19,16 +48,18 @@ const Modal = ({ children, ...props }: ModalProps): JSX.Element => {
         transparent
         {...props}
       >
-        <TouchableWithoutFeedback onPress={onClose}>
-          <S.Background />
+        <TouchableWithoutFeedback onPress={closeModal}>
+          <Animated.View style={{ flex: 1, opacity }}>
+            <S.Background />
+          </Animated.View>
         </TouchableWithoutFeedback>
         <S.Container style={{ height: 289 }}>
           <S.CloseWrapper>
-            <S.ButtonClose activeOpacity={0.7} onPress={onClose} />
+            <S.ButtonClose activeOpacity={0.7} onPress={closeModal} />
           </S.CloseWrapper>
           {!!title && (
             <S.TitleWrapper>
-              <S.Title>{title}</S.Title>
+              <S.Title style={titleStyle}>{title}</S.Title>
             </S.TitleWrapper>
           )}
           {children}
